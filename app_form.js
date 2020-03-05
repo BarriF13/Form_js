b5e7a794e9a612edec3787296485a2709e6d7086
Student <student@Makerss-MacBook-Air-5.local>
2020-03-05 15:15:29 +0000

    Edit app_form

--- app_form.js
+++ app_form.js
@@ -12,20 +12,39 @@ var form ={
   pass1: document.getElementById("pass1"),
   pass2: document.getElementById("pass2"),
 };
+
 //now we are giving an event
 form.register.addEventListener("submit", checkform);
+form.pass1.addEventListener("keypress", nospace);
+form.pass2.addEventListener("keypress", nospace);
+form.pass1.addEventListener("keyup", passwordstrength);
+
+
+
 //checking the email format//setting up RegEx
 var reemail=/^[a-z0-9]+@[a-z]+\.+[a-z]{2,4}$/;//"test1234@gmail.com"
 
 function checkform(e){
   //alert(form.email.value);//getting the input email
+  //evaluate email form
 
+  var msg ="";
   if(!reemail.test(form.email.value)){
 
-    alert('This is not a correct email address')
-  }
-  else {
-    alert('Your email is accepted')
+    msg+="Your email";
   }
   e.preventDefault();
+}
+
+function nospace(e){
+  //space charcode is 32
+  //alert(e.charCode);
+  if (e.charCode == 32){
+    e.preventDefault();
+  }
+}
+
+function passwordstrength(e){
+  var pass = form.pass1.value;
+  alert(pass);
 }
