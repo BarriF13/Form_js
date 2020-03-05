b7ef1f1ba77ee3dee06cfd0fea03e5914d8eaab7
Student <student@Makerss-MacBook-Air-5.local>
2020-03-05 13:45:21 +0000

    Set up the app files

--- app_form.js
+++ app_form.js
@@ -1,1 +1,21 @@
 // alert("ouch")
+// var form = document.getElementById("register");
+
+// form.onsubmit = function(){
+//   alert("it is working");
+//   return false;
+// }
+//selecting the form --
+var form ={
+  register: document.getElementById("register"),
+  email: document.getElementById("email"),
+  pass1: document.getElementById("pass1"),
+  pass2: document.getElementById("pass2"),
+};
+//now we are giving an event
+form.register.addEventListener("submit", checkform);
+
+function checkform(e){
+  alert("it is working");
+  e.preventDefault();
+}
